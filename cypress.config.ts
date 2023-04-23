e4ed7e31fb5f2064da528fc9dfcb48d51f32c5da
eitan <eitanlich2000@gmail.com>
2023-04-23 18:25:16 +0300

    Added the required tests, expect for tab rendering

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://localhost:4173",
+    baseUrl: "http://localhost:4173/open-map-data-multi-layers-demo/?lat=34.844526&lng=-82.401078&zoom=10",
   },
 });
