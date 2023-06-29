7937aad4d85a12f1e6d5a72f50b165a3937f4352
Jim Ciallella <1777776+allella@users.noreply.github.com>
2023-06-28 23:47:42 -0400

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,15 +2,19 @@
   <div
     class="h-full mx-auto max-w-sm flex flex-col justify-center items-center space-y-4 text-xl text-center"
   >
-    <h1 class="py-4 text-2xl">Greenville OpenMap Demo</h1>
+    <h1 class="py-4 text-2xl">
+      Upstate / Greenville SC Open Data Map Layers Demo
+    </h1>
 
     <p>
       This app is maintained by
-      <a class="link" href="https://codeforgreenville.org">
-        Code for Greenville
+      <a class="link" href="https://hackgreenville.com/join-slack">
+        <em>HackGreenville Labs</em> team in the #hg-labs Slack channel
       </a>
       and contains data from the
-      <a class="link" href="https://data.openupstate.org/">OpenData</a>
+      <a class="link" href="https://data.openupstate.org/map-layers"
+        >Upstate SC Open Map Layer Data</a
+      >
       initiative.
     </p>
     <p>
@@ -18,14 +22,14 @@
       <a class="link" href="https://data.openupstate.org/contribute">
         you can contribute
       </a>
-      to the OpenData project!
+      to the Open Data project!
     </p>
     <p>
       This app is open source and contributions are highly encouraged! You can
       find the source code for this site on
       <a
         class="link"
-        href="https://github.com/codeforgreenville/open-map-data-multi-layers-demo"
+        href="https://github.com/hackgvl/open-map-data-multi-layers-demo"
       >
         Github
       </a>
