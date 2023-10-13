b3f9307b2a5d3d494e13dcc0af576136f5240a4b
oliviasculley <olivia@sculley.dev>
2023-10-13 18:30:11 -0400

    add sourcemaps to production builds

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    build: { sourcemap: true },
     plugins: [vue()],
     resolve: {
       alias: {
