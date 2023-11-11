88991e9276cb889b4091e7110a71a1c972b92f39
alex <alexgrimes.se@gmail.com>
2023-11-11 16:49:01 -0500

    fixed linter error

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/:pathMatch(.*)',
+      path: "/:pathMatch(.*)",
       name: "404",
       component: NotFoundView,
     },
