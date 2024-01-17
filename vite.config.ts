5ea252d2db3ec20031d9c944ba0205e0b29c1406
oliviasculley <olivia@sculley.dev>
2024-01-16 21:55:02 -0500

    add port settings to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    server: {
+      port: 4173,
+    },
     build: { sourcemap: true },
     plugins: [vue()],
     resolve: {
